a81d8f30cf26b9ceedc80b67905f78eae2860b04
Anuradha Mishra <94345789+anuradhadesigner@users.noreply.github.com>
2022-04-05 14:48:06 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://developer:developer123@cluster0.r3psx.mongodb.net/netlix-node?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://developer:developer123@cluster0.r3psx.mongodb.net/netflix-node?retryWrites=true&w=majority")
